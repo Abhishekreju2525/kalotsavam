dea8cad69ee3a411789f586fc91782570e8788fc
Abhishekreju2525 <abhishekreju007@gmail.com>
2021-09-12 12:54:40 +0530

    navbarbg changeonscroll update

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 window.onscroll = () => {
   const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-t'; else nav.className = 'navbar navbar-expand-lg fixed-top navbar-light bg-light';
+  if(this.scrollY <= 1) nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-t'; else nav.className = 'navbar navbar-expand-lg fixed-top navbar-light bg-light';
 };
 
 
